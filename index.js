205c9022ffefaec25e7251439bb744e135b33bbc
KUNDEM NAGA SAI TULASIRAM <k.tulasiram444@gmail.com>
2020-07-14 09:24:07 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/contact', require('./routes/api/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ routes/api/contacts.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const Contact = require('../../models/Contact');
+
+// @route    GET api/contact
+// @desc     Get all contacts
+// @access   Public
+router.get('/', async (req, res) => {
+    try {
+        const contacts = await Contact.find().sort({ date: -1 });
+        res.json(contacts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route    Post api/contact
+// @desc     Add contact
+// @access   Public
+router.post(
+    '/',
+    [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+        check('email', 'Email is required')
+            .not()
+            .isEmpty(),
+        check('subject', 'Subject is required')
+            .not()
+            .isEmpty(),
+        check('message', 'Message is required')
+            .not()
+            .isEmpty(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, subject, message } = req.body;
+
+        try {
+            contact = new Contact({ name, email, subject, message });
+            await contact.save();
+            res.json(contact);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
+
+module.exports = router;
